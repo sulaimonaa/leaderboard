18f9b11f968f3d2d0848e1d266e19995e63d43de
Sulaimon Azeez Ayodeji <sulaimonaa@gmail.com>
2023-07-04 11:11:22 +0100

    fix linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BundleAnalyzerPlugin =
-  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
   mode: 'development',
